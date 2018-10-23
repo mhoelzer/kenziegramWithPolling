1e00a7727327a31d5e2ac60d75d309ed632790e0
Michelle Hoelzer <hoelzermm@gmail.com>
2018-10-23 15:07:37 -0400

    finished errors i think idk but im done

--- public/index.js
+++ public/index.js
@@ -1,13 +1,15 @@
 let mostRecentTimestamp = Date.now();
 let url = window.location.href;
-// let failures = 0;
+let failures = 0;
 let interval = 5000;
 
 function fetchUploadsAndTimestamps(timestamp) {
-    // failures++;
-    // if(failures > 2) {
-    //     clearTimeout
-    // }
+    failures++;
+    if(failures > 2) {
+        clearTimeout(setTimeout());
+        alert("ERROR ERROR YOU'RE A FAILURE");
+        return;
+    }
     fetch("/latest", {
         method: "POST",
         body: JSON.stringify({ "after": mostRecentTimestamp }),
@@ -15,13 +17,13 @@ function fetchUploadsAndTimestamps(timestamp) {
             "Content-Type": "application/json"
         }
     })
-        .then(response => response.json()) // parameter; parses response
+        .then(response => response.json()) // parameter; parses response; the return is implicit
         .then(serverResponse => {
             console.log(serverResponse)
-            return serverResponse // do returns for getting not undefiend
+            return serverResponse // do returns to not get undefiend and give stuff to the other .thens
         }) // actual js obj that server sent back 
         .then(objectThing => {
-            // failures = 0;
+            failures = 0;
             mostRecentTimestamp = objectThing.timestamp;
             const arrayOfImagePaths = objectThing.images.reverse();
             for(let imagePath of arrayOfImagePaths) {
@@ -29,7 +31,7 @@ function fetchUploadsAndTimestamps(timestamp) {
                 imageHere.src = `${imagePath}`;
                 document.getElementById("photos").insertBefore(imageHere, document.getElementById("photos").firstChild)
             }
-            setTimeout(fetchUploadsAndTimestamps, interval)
+            setTimeout(fetchUploadsAndTimestamps, interval) // this goes inside so it gets done in the right order with the rest of the .then info here 
         })
 };
 
